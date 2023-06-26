5384d48e04c649cf66ff363b0c31f4ab89408199
agneum14 <agneum14@gmail.com>
2023-06-26 16:54:31 -0400

    install nvim-autopairs

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,35 +1,40 @@
 return {
-	'tpope/vim-fugitive',
-	{
-		'ibhagwan/fzf-lua',
-		branch = 'main',
-		depencies = { 'nvim-tree/nvim-web-devicons' }
-	},
-	{
-		'nvim-treesitter/nvim-treesitter',
-		build = ':TSUpdate'
-	},
-	 {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    dependencies = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
-      {                                      -- Optional
-        'williamboman/mason.nvim',
-        build = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end,
-      },
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    'tpope/vim-fugitive',
+    {
+        'ibhagwan/fzf-lua',
+        branch = 'main',
+        depencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate'
+    },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                            -- Optional
+                'williamboman/mason.nvim',
+                build = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},     -- Required
-      {'hrsh7th/cmp-nvim-lsp'}, -- Required
-      {'L3MON4D3/LuaSnip'},     -- Required
-    }
-  },
-  'mfussenegger/nvim-jdtls',
-  'ggandor/leap.nvim',
-	'dracula/vim'
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
+        }
+    },
+    'mfussenegger/nvim-jdtls',
+    'ggandor/leap.nvim',
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        opts = {} -- this is equalent to setup({}) function
+    },
+    'dracula/vim'
 }
