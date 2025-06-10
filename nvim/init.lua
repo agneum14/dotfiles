c6bef4923ad627b71a19cb49e975131493da4008
Andrew G. Neumann <agneum14@gmail.com>
2025-06-10 14:11:53 -0400

    Update LSP configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,13 @@ vim.g.mapleader = ' '
 
 require('lazy-setup')
 require('set')
+
+servers = { 'pylsp', 'lua_ls', 'html', 'tailwindcss', 'gopls' }
+for _, server in ipairs(servers) do
+    vim.lsp.enable(server)
+end
+require("mason").setup()
+require("mason-lspconfig").setup {
+    ensure_installed = servers,
+    automatic_enable = false
+}
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,5 +1,11 @@
 return {
+    'neovim/nvim-lspconfig',
     'tpope/vim-fugitive',
+    {
+        "mason-org/mason.nvim",
+        "mason-org/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    },
     {
         'ibhagwan/fzf-lua',
         branch = 'main',
@@ -10,30 +16,6 @@ return {
         build = ':TSUpdate',
         dependencies = 'JoosepAlviste/nvim-ts-context-commentstring'
     },
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        dependencies = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
-            {                            -- Optional
-                'williamboman/mason.nvim',
-                build = function()
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
-            },
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
-
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' },     -- Required
-            { "rafamadriz/friendly-snippets" },
-            { "https://github.com/saadparwaiz1/cmp_luasnip" },
-        }
-    },
-    'mfussenegger/nvim-jdtls',
-    { "folke/neodev.nvim", opts = {} },
     'ggandor/leap.nvim',
     {
         'windwp/nvim-autopairs',
@@ -42,15 +24,73 @@ return {
     },
     'terrortylor/nvim-comment',
     'xiyaowong/transparent.nvim',
-    {
-        "folke/trouble.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
-
- -- COLORSCHEMES
+    -- COLORSCHEMES
     'dracula/vim',
     'folke/tokyonight.nvim',
     'overcache/NeoSolarized',
     'ellisonleao/gruvbox.nvim',
     'rose-pine/neovim',
+    {
+        'saghen/blink.cmp',
+        dependencies = { 'rafamadriz/friendly-snippets' },
+        version = '1.*',
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            keymap = { preset = 'default' },
+
+            appearance = {
+                nerd_font_variant = 'mono'
+            },
+
+            completion = { documentation = { auto_show = false } },
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
+
+            fuzzy = { implementation = "prefer_rust_with_warning" }
+        },
+        opts_extend = { "sources.default" }
+    },
+    {
+        "folke/trouble.nvim",
+        opts = {}, -- for default options, refer to the configuration section for custom setup.
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+        },
+    },
+    {
+      'stevearc/conform.nvim',
+      opts = {},
+    }
 }
